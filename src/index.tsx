67ff362aa1abfc305eb947e6c59edb9226f328d3
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-02 18:48:30 +0200

    start with components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+import App from "./components/App"
 
-const App = () => {
-  return <div>Hello</div>
-}
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <React.StrictMode>
